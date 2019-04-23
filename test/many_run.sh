492af619cad1b6b8cb0daf7519543bc208a6bdb0
Jean-Didier Garaud <jean-didier.garaud@onera.fr>
2019-04-23 17:17:46 +0200

    Allow many_run.sh to play nb_players

--- test/many_run.sh
+++ test/many_run.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
-nb=$*
+nb=$1
+nb_players=$2
 
 if [ -z "$nb" ]; then
     nb=10
 fi
 
+if [ -z "$nb_players" ]; then
+    nb_players=2
+fi
+
 for i in $(seq 1 $nb); do
-    score=$(hanabi --ai=Cheater |grep score |sed -e 's/.*is //')
+    score=$(hanabi -n $nb_players --ai=Cheater |grep score |sed -e 's/.*is //')
     echo $score
     if [ $score -lt 25 ]; then
         cp autosave.py gamelost$i.py
