14b9c185ca493b15d2543d80601522073fd378c9
Jean-Didier Garaud <jean-didier.garaud@onera.fr>
2019-04-23 18:21:37 +0200

    unittest HandTest: contribution of Bernard Gorzkowski

--- test/hanabi_unittest.py
+++ test/hanabi_unittest.py
@@ -34,6 +34,26 @@ class CardTest(unittest.TestCase):
 class HandTest(unittest.TestCase):
     # test __special__ functions
     
+    def setUp(self):
+        self.deck1 = hanabi.deck.Deck()
+        self.hand1 = hanabi.deck.Hand(self.deck1)
+        self.deck1.shuffle()
+
+        self.deck3 = hanabi.deck.Deck()
+        self.hand3 = hanabi.deck.Hand(self.deck3,1)
+
+    def test_basic_hand(self):
+        self.assertEqual(self.hand3.__str__(),hanabi.deck.Card(hanabi.deck.Color.Red,1).str_color())
+
+    def test_len(self):
+        self.assertEqual(5, len(self.hand1))
+    
+    def test_shuffle(self):
+        self.deck1.shuffle()
+        mem = str(self.deck1)[0:len(repr(self.hand1))]
+        self.hand2 = hanabi.deck.Hand(self.deck1)
+        self.assertEqual(str(self.hand2), mem)
+
 
     # test normal functions
     pass
